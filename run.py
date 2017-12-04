0a82043c3bdbf7fb7fbd474fe31dd97674bf8ab5
dalangzhonghangxing <553778439@qq.com>
2017-12-04 10:20:23 +0800

    使用神经网络+w2v跑了个结果

--- run.py
+++ run.py
@@ -16,11 +16,24 @@ def generateRelationByRefD():
                 relationMatrix[i][j] = -refd
     np.save("relationMatrix.npy",relationMatrix)
 
-generateRelationByRefD()
-br.getDirectedRelation()
+# generateRelationByRefD()
+# br.getDirectedRelation()
 
-# init = br.init()
-# matrix = init[0]
-# zeta = init[1]
-# refd = br.RefD(matrix, 5, 185)
-# print(refd)
+# 读取使用神经网络生成的关系
+def generateRelationByW2V():
+    file = open("bikeWords.txt", 'r', encoding='utf-8')
+    line = file.readline()
+    bikewords = line.split(" ")
+
+    delta = 300
+    ans = np.load("w1plusw2_multipliedby_w1plusw2.npy")
+    # print(ans.shape)
+    k = 0
+    for i in range(0, ans.shape[0]-1):
+        for j in range(0, ans.shape[0] - 1):
+            if ans[i][j] > delta :
+                print(ans[i][j], bikewords[i]+"--->"+bikewords[j])
+                k+=1
+            # elif ans[i][j] < -delta:
+            #     print(ans[i][j], bikewords[j] + "--->" + bikewords[i])
+    print("relation number:"+k)
